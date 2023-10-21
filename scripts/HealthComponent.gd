44b4c1361139e6a9ee35f1220aa3e6340b5573fb
Yanis Dubois <yanis.dubois@etu.u-bordeaux.fr>
2023-10-21 11:25:12 +0200

    blood

--- scripts/HealthComponent.gd
+++ scripts/HealthComponent.gd
@@ -2,16 +2,22 @@ extends Node2D
 
 class_name HealthComponent
 
-@export var health = 10
-@onready var blood = preload("res://scene/blood.tscn")
+@export var BLOOD = preload("res://scene/blood.tscn")
+@export var health = 1
+var character
+var level
 
 func _ready():
+	character = get_parent()
+	level = character.get_parent()
 	pass
 
 func take_damage(dmg : int):
 	health -= dmg
 	print("Took damage :", dmg )
-	if health<0:
-		var bld = blood.instantiate()
-		get_parent().get_parent().add_child(bld)
-		get_parent().queue_free()
+	if health<=0:
+		level.remove_child(character)
+		character.call_deferred("free")
+		var blood = BLOOD.instantiate()
+		blood.position = character.position
+		level.add_child(blood)
--- scripts/Hitbox.gd
+++ scripts/Hitbox.gd
@@ -2,12 +2,11 @@ extends Area2D
 
 class_name HitBox
 
-var damage = 10
+var damage = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
