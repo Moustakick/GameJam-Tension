a8469bb07ca16d57a9d5d28288d23e88c6610444
Yanis Dubois <yanis.dubois@etu.u-bordeaux.fr>
2023-10-21 14:55:55 +0200

    attack with mouse click

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,33 +4,61 @@ extends CharacterBody2D
 
 @onready var health = $HealthComponent as HealthComponent
 @onready var sword = preload("res://scene/sword.tscn")
-@onready var center_marker = $CenterMarker
-@onready var sword_marker = $CenterMarker/SwordMarker
+@onready var center_marker_key = $CenterMarkerKey
+@onready var sword_marker_key = $CenterMarkerKey/SwordMarkerKey
+@onready var center_marker_mouse = $CenterMarkerMouse
+@onready var sword_marker_mouse = $CenterMarkerMouse/SwordMarkerMouse
 
-var sw;
-var direction;
-var last_rotation = 0;
+var sw
+var is_sw_key = false
+var is_sw_mouse = false
+var direction
+var last_rotation = 0
 
 func get_input():
 	direction = Input.get_vector("left", "right", "up", "down")
+	var mouse_pos = get_viewport().get_mouse_position()
+	var global_pos = get_global_transform_with_canvas().get_origin()
 	
+	# update sword position key
 	if direction == Vector2(0,0):
-		center_marker.rotation = last_rotation
+		center_marker_key.rotation = last_rotation
 	else:
-		center_marker.rotation = 0
-		center_marker.rotate(direction.angle())
-		last_rotation = center_marker.rotation
+		center_marker_key.rotation = 0
+		center_marker_key.rotate(direction.angle())
+		last_rotation = center_marker_key.rotation
+	
+	# update sword position key
+	var mouse_direction = mouse_pos - global_pos
+	center_marker_mouse.rotation = 0
+	center_marker_mouse.rotate(mouse_direction.angle())
 	
 	velocity = direction * SPEED
+
+func _input(event):
+	# attack with key board
+	if not is_sw_mouse:
+		if event.is_action_pressed("attack_key"):
+			is_sw_key = true
+			sw = sword.instantiate()
+			sword_marker_key.add_child(sw)
+		if is_sw_key and event.is_action_released("attack_key"):
+			sw.queue_free()
+			is_sw_key = false
+	
+	# attack with mouse
+	if not is_sw_key:
+		if event.is_action_pressed("attack_mouse"):
+			is_sw_mouse = true
+			sw = sword.instantiate()
+			sword_marker_mouse.add_child(sw)
+		if is_sw_mouse and event.is_action_released("attack_mouse"):
+			sw.queue_free()
+			is_sw_mouse = false
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
-	if event.is_action_pressed("attack"):
-		sw = sword.instantiate()
-		sword_marker.add_child(sw)
-	if event.is_action_released("attack"):
-		sw.queue_free()
 
 func _physics_process(delta):
 	get_input()
