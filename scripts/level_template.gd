ab5b27ab916a6fa3058b12c83fd8e4022f1a0984
Yanis Dubois <yanis.dubois@etu.u-bordeaux.fr>
2023-10-22 15:21:46 +0200

    game over menu ++

--- scripts/level_template.gd
+++ scripts/level_template.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @onready var player = $Player
+@onready var end_screen_timer = $EndScreenTimer
+
+var is_first = true
+var screen_lock = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,13 +12,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if !is_instance_valid(player) and is_first:
+		end_screen_timer.start()
+		is_first = false
 
 func _unhandled_input(event):
 	if event.is_action_pressed("reload"):
 		get_tree().reload_current_scene()
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().change_scene_to_file("res://scene/levels/menu_principal.tscn")
-	elif !is_instance_valid(player):
+	if !screen_lock:
 		if event is InputEventKey or event is InputEventMouseButton:
 			get_tree().reload_current_scene()
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://scene/levels/menu_principal.tscn")
+
+func _on_end_screen_timer_timeout():
+	screen_lock = false
