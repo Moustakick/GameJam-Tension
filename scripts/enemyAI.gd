7d60ad5136221fcdb113ae2db432c9ef11ab77b9
Yanis Dubois <yanis.dubois@etu.u-bordeaux.fr>
2023-10-22 11:11:17 +0200

    mechant qui bouge

--- scripts/enemyAI.gd
+++ scripts/enemyAI.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var enemy_detection_zone = $EnemyDetectionZone
 @onready var escape_timer = $EscapeTimer
+@onready var move_timer = $RandomMoveTimer
 
 # states
 enum State {
@@ -14,38 +15,41 @@ var state = State.WALK_ARROUND
 var enemy: Enemy = null
 var player: Player = null
 var direction: Vector2
+var is_walking = true
+var random = RandomNumberGenerator.new()
 
 func set_state(new_state):
 	if new_state != state:
 		state = new_state
+		
 		if new_state == State.PANIC:
 			enemy.SPEED *= 3
 		else:
 			enemy.SPEED /= 3
-		emit_signal("state_changed", state)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	enemy = get_parent()
+	move_timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	match state:
 		State.WALK_ARROUND:
-			pass
-		State.ESCAPE:
-			if player != null:
+			if is_walking:
 				enemy.velocity = direction * enemy.SPEED
 				enemy.move_and_slide()
-			else :
-				set_state(State.ESCAPE)
+			pass
+		State.ESCAPE:
+			enemy.velocity = direction * enemy.SPEED
+			enemy.move_and_slide()
+			pass
 		State.PANIC:
 			if player != null:
 				direction = (enemy.position - player.position).normalized()
 				enemy.velocity = direction * enemy.SPEED
 				enemy.move_and_slide()
-			else :
-				set_state(State.ESCAPE)
+			pass
 
 func _on_enemy_detection_zone_body_entered(body: Node):
 	if body.is_in_group("player"):
@@ -60,3 +64,9 @@ func _on_enemy_detection_zone_body_exited(body):
 
 func _on_escape_timer_timeout():
 	set_state(State.WALK_ARROUND)
+
+func _on_random_move_timer_timeout():
+	if state == State.WALK_ARROUND:
+		is_walking = !is_walking
+		if is_walking:
+			direction = Vector2(random.randf_range(-1, 1), random.randf_range(-1, 1)).normalized()
