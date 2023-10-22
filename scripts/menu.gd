eb6965626ad509700a142260d14452a31701d679
Yanis Dubois <yanis.dubois@etu.u-bordeaux.fr>
2023-10-22 10:08:35 +0200

    level template

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,20 +5,12 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scene/levels/level_test.tscn")
-	print("pressed")
-
-
-func _on_area_2d_area_entered(area):
-	print("Aïe")
-
+	get_tree().change_scene_to_file("res://scene/levels/level_template.tscn")
 
 func _on_button_2_pressed():
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
