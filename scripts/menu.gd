9b2f68b8d8eaef79f2de34c8fedd00b7df328de2
Yanis Dubois <yanis.dubois@etu.u-bordeaux.fr>
2023-10-22 17:14:03 +0200

    level 1-3

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -14,7 +14,10 @@ func _unhandled_input(event):
 		get_tree().quit()
 
 func _on_menu_enemy_level_1_enemy_death():
-	get_tree().change_scene_to_file("res://scene/levels/level_template.tscn")
+	get_tree().change_scene_to_file("res://scene/levels/level_1.tscn")
 
 func _on_menu_enemy_level_2_enemy_death():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scene/levels/level_2.tscn")
+
+func _on_menu_enemy_level_3_enemy_death():
+	get_tree().change_scene_to_file("res://scene/levels/level_3.tscn")
